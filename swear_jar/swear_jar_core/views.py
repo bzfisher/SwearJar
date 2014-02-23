4d22482502d59f8e8c5641f2ffe3486bf3e444f2
davidrapoport <drapoport847@gmail.com>
2014-02-23 01:05:42 -0500

    twilio round 3

--- swear_jar/swear_jar_core/views.py
+++ swear_jar/swear_jar_core/views.py
@@ -64,7 +64,6 @@ def amountDue(request):
 		return HttpResponse((user.swearCount)/10.0)
 	else:
 		return HttpResponse(0)
-import requests
 from twilio.rest import TwilioRestClient 
 from django.db import models
  
@@ -79,20 +78,21 @@ def send_text(count, number):
      cnt = (int(count) / 10.0)
      message = "You swore " + count+" times today. Donate "+str(cnt)+	 	"dollar(s) to charity, or Mom will hear about what you said!"
   	# put your own credentials here 
-  	f = open("Twilio",r).readLines()
-	ACCOUNT_SID =  f[0]
-  	AUTH_TOKEN = f[1] 
+     f = open("Twilio",r).readLines()
+     ACCOUNT_SID =  f[0]
+     AUTH_TOKEN = f[1] 
  
-  	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+     client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
  
-  	client.messages.create( to=number, from_="+14387937802", 		body=message  )
+     client.messages.create( to=number, from_="+14387937802",body=message  )
 
 @csrf_exempt  
-def send_all():
-	for u in Models.User.people.all():
+def send_all(request):
+	for u in User.people.all():
 		send_text(u.swearCount,u.phoneNumber)
+	return HttpResponse(0)
 @csrf_exempt
-def send_text_mom(u):
+def send_text_mom(u,request):
 	if not u.swearCount==0:
 		message = "Look ma!! Your darling child swore "+u.swearCount+" today! Want a list? Here ya go "+u.swears
 		f = open("Twilio",r).readLines()
@@ -101,12 +101,13 @@ def send_text_mom(u):
  
   		client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
  
-  		client.messages.create( to=u.momsNumber, from_="+14387937802", 		body=message  )
+  		client.messages.create( to=u.momsNumber, from_="+14387937802",body=message  )
 
 @csrf_exempt
 def send_all_mom():
-	for u in Models.User.people.all():
+	for u in User.people.all():
 		send_text_mom(u)
 		u.swearCount=0
+	return HttpResponse(0)
 
 
