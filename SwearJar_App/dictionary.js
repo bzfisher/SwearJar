bd056f8a8d10b64bc1d8976eff06952b4532829f
alecblumenfeld <Alec@wpa015015.wireless.mcgill.ca>
2014-02-22 22:00:58 -0500

    dictoary

---
+++ SwearJar_App/dictionary.js
@@ -0,0 +1,9 @@
+
+var str = "The rain in SPAIN stays mainly in the plain fucker"; 
+var res = str.match(/fuck/g)&&str.match(/shit/g)&&str.match(/fuck/g)&&
+str.match(/cunt/g)&&str.match(/ass/g)&&str.match(/cum/g)&&
+str.match(/whore/g)&&str.match(/slut/g)&&str.match(/fag/g)&&
+str.match(/kike/g)&&str.match(/spick/g)&&str.match(/dick/g)&&
+str.match(/bastard/g)&&str.match(/fuck/g)&&:
+<p id="demo"> SWAP </p>
+document.getElementById("demo").innerHTML=res;
