099fb62f45ed7b2a3d07f608e1ef465c92019923
davidrapoport <drapoport847@gmail.com>
2014-03-14 14:14:21 -0400

    Scanner update after reset

--- SwearJar_chrome/scanner.js
+++ SwearJar_chrome/scanner.js
@@ -1,4 +1,3 @@
-
 var word = new Array();
 
 function sendMethod(){
@@ -13,22 +12,20 @@ function sendMethod(){
 			
 	}
 	
-	var str
+	var str;
 	for(var i=0;var<word.length;var++){
 		str= str+word[i];
 	}
-	if(true){
-		$.post("http://107.170.73.156:8001/userswore.html"),
-        {username:owner
-        swearword:str},
+	if(checkword(str)){
+		$.post("http://107.170.73.156:8001/userswore.html", {username:owner, swearword:str},
         function() {
-            //put count cookie here? 
-        }
 	cnt = parseInt(count)+1;
         document.cookie="count="+cnt+"; expires=Thu, 18 Dec 2015 12:00:00 GMT; path=/";
-	}
+	});
 word.length=0;
 }
+
+
 document.onkeypress = function(e) {
     e = e || window.event;
     var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
@@ -41,6 +38,25 @@ document.onkeypress = function(e) {
 	word.push(String.fromCharCode(charCode));
 	}
     }
-};
 
+}
+function checkword = function(e){
+	var str = e; 
+var res = str.match(/fuck/g)||str.match(/shit/g)||str.match(/fuck/g)&&
+str.match(/cunt/g)||str.match(/ass/g)||str.match(/cum/g)||
+str.match(/whore/g)||str.match(/slut/g)||str.match(/fag/g)||
+str.match(/kike/g)||str.match(/spick/g)||str.match(/dick/g)||
+str.match(/bastard/g)||str.match(/fuck/g)||:
+var temp = res;
+var val = res.charAt(0);
+var found= false;
+if(val>0){
+	found=true;
+}
+if(found==true){
+	sendMethod(str);
 
+}
+}
+
+;
